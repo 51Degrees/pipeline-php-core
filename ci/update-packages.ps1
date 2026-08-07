1d908831c1d764366438cdb8746b60445ccf843d
Alexander Popel <alexander.popel@postindustria.com>
2026-08-07 09:53:11 +0300

    ci: fix update-packages cleanup

--- ci/update-packages.ps1
+++ ci/update-packages.ps1
@@ -1,19 +1,18 @@
 param (
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName,
-    [Parameter(Mandatory=$true)]
-    [string]$OrgName,
+    [Parameter(Mandatory)][string]$RepoName,
+    [Parameter(Mandatory)][string]$OrgName,
     [bool]$DryRun = $false
 )
+$ErrorActionPreference = 'Stop'
 
-Write-Output "Downloading latest javascript-templates..."
+Write-Host "Downloading latest javascript-templates..."
 Invoke-WebRequest "https://github.com/$OrgName/javascript-templates/archive/refs/heads/main.zip" -OutFile javascript-templates.zip
 
-Write-Output "Extracting the archive..."
+Write-Host "Extracting the archive..."
 Expand-Archive javascript-templates.zip -DestinationPath .
 
-Write-Output "Updating the package directory..."
-Move-Item -Path javascript-templates-main/*.mustache -Destination $RepoName/javascript-templates -Force
+Write-Host "Updating the package directory..."
+Move-Item -Path javascript-templates-main/*.mustache -Destination "$RepoName/javascript-templates" -Force
 
-Write-Output "Cleaning up..."
-Remove-Item javascript-templates.zip, javascript-templates-main -Recurse
+Write-Host "Cleaning up..."
+Remove-Item -Recurse -Force javascript-templates.zip, javascript-templates-main
